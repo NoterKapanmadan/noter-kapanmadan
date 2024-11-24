1b6b205a97574d69f5d2f7babc214e7b63949d6d
Oğuzhan Genç <gnc.oguzhan.33@gmail.com>
2024-11-24 16:37:36 +0300

    feat(webpage): add db connection

--- webpage/src/app/page.js
+++ webpage/src/app/page.js
@@ -1,10 +1,19 @@
 import VehicleMarketplace from "@/components/layout/HomePage";
 import { getRandomCars } from "@/utils/placeholderCar";
+import { query } from "@/lib/db";
 
 
 export default async function HomePage() {
   let vehicleAds = await getRandomCars();
+  query
   console.log(vehicleAds);
+  
+  try {
+    const result = await query('SELECT NOW()');
+    console.log('Connection successful:', result.rows[0]);
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
 
   return(<VehicleMarketplace vehicleAds={vehicleAds}/>);
 }
