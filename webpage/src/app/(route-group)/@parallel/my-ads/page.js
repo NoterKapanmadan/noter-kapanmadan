47327cd1365a3c4ec9aee8c283e2ada3f598758e
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2024-12-20 00:37:48 +0300

    feat(favorites): make Tabs dynamic with URL

---
+++ webpage/src/app/(route-group)/@parallel/my-ads/page.js
@@ -0,0 +1,8 @@
+import { Suspense } from "react"
+import LoadingSkeleton from "@/components/layout/LoadingSkeleton"
+
+export default function MyAdsParallel() {
+  return (
+    <div>MyAds</div>
+  )
+}
