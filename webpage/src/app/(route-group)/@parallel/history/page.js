20a4b523592574dc7d5e2cbba4209d1d00b1aa2b
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2024-12-20 14:17:57 +0300

    feat(webpage): add history page

--- webpage/src/app/(route-group)/@parallel/history/page.js
+++ webpage/src/app/(route-group)/@parallel/history/page.js
@@ -1,5 +1,49 @@
-export default function HistoryParalled() {
+import { Suspense } from "react"
+import LoadingSkeleton from "@/components/layout/LoadingSkeleton"
+
+export default function HistoryParallel({ searchParams }) {
+  const {
+    title = '',
+    minPrice = '',
+    maxPrice = '',
+    date = '',
+    location = '',
+    brand = '',
+    model = '',
+    minYear = '',
+    maxYear = '',
+    maxKm = '',
+    gear_type = '',
+    fuel_type = '',
+    latitude = '',
+    longitude = '',
+    maxDistance = '',
+    page = '1'
+  } = searchParams
+
+  const filters = {
+    title,
+    minPrice,
+    maxPrice,
+    date,
+    location,
+    brand,
+    model,
+    minYear,
+    maxYear,
+    maxKm,
+    gear_type,
+    fuel_type,
+    latitude,
+    longitude,
+    maxDistance,
+    page: Number(page),
+  }
+  
   return (
-    <div>History</div>
+    <Suspense fallback={<LoadingSkeleton />}>
+      History
+      {/* <Favorites filters={filters} /> */}
+    </Suspense>
   )
 }
