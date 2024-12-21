dd12e975cbe5d5e7294a303a985669776cbc63d4
Furkan Emre Kolukırık <emre.kolukirik@ug.bilkent.edu.tr>
2024-12-21 19:28:03 +0300

    feat(messaging): add actions

--- webpage/src/app/chat/[receiver]/page.js
+++ webpage/src/app/chat/[receiver]/page.js
@@ -1,6 +1,10 @@
 import ChatComponent from "@/components/layout/ChatComponent";
+import { getChatRoom } from "@/app/actions";
 
 export default async function Chat({ params }) {
+
+  const chatRoom = await getChatRoom(params.receiver);
+
 
   return (
     <ChatComponent receiver={params.receiver} />
