9ef74254f5ffe43ae9332cfe280087a4be6396a6
Erdem Aydın <erdemaydin7139@gmail.com>
2024-12-23 12:42:01 +0300

    feat(myads): add myads

--- webpage/src/app/(route-group)/@parallel/my-ads/page.js
+++ webpage/src/app/(route-group)/@parallel/my-ads/page.js
@@ -1,8 +1,49 @@
+import MyAds from "@/components/layout/MyAds"
 import { Suspense } from "react"
 import LoadingSkeleton from "@/components/layout/LoadingSkeleton"
 
-export default function MyAdsParallel() {
+export default function FavoritesParallel({ searchParams }) {
+  const {
+    title = '',
+    minPrice = '',
+    maxPrice = '',
+    date = '',
+    location = '',
+    brand = '',
+    model = '',
+    minYear = '',
+    maxYear = '',
+    maxKm = '',
+    gear_type = '',
+    fuel_type = '',
+    latitude = '',
+    longitude = '',
+    maxDistance = '',
+    page = '1'
+  } = searchParams
+
+  const filters = {
+    title,
+    minPrice,
+    maxPrice,
+    date,
+    location,
+    brand,
+    model,
+    minYear,
+    maxYear,
+    maxKm,
+    gear_type,
+    fuel_type,
+    latitude,
+    longitude,
+    maxDistance,
+    page: Number(page),
+  }
+  
   return (
-    <div>MyAds</div>
+    <Suspense fallback={<LoadingSkeleton />}>
+      <MyAds filters={filters} />
+    </Suspense>
   )
 }
