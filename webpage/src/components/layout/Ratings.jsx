88000118398b23bc2b5473526a390acb7241a6c0
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2025-01-01 22:46:09 +0300

    fix(rating): fix layout

--- webpage/src/components/layout/Ratings.jsx
+++ webpage/src/components/layout/Ratings.jsx
@@ -33,26 +33,30 @@ export default function Ratings({ fullname, ratings }) {
             </div>
           </DialogDescription>
         </DialogHeader>
-          {ratings.evaluations.length > 0 ? ratings.evaluations.map((evaluation, index) => (
+          {ratings.evaluations.length > 0 ? (
             <div className="grid gap-4 py-4 max-h-80 overflow-scroll">
-              {index > 0 && <hr />}
-              <div key={evaluation.comment_date} className="flex flex-col gap-2">
-                <div className="flex flex-col gap-1">
-                  <Stars point={evaluation.point} />
-                  <span className="text-sm">{evaluation.comment}</span>
+              {ratings.evaluations.map((evaluation, index) => (
+               <>
+                {index > 0 && <hr />}
+                <div key={evaluation.comment_date} className="flex flex-col gap-2">
+                  <div className="flex flex-col gap-1">
+                    <Stars point={evaluation.point} />
+                    <span className="text-sm">{evaluation.comment}</span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Avatar className="border">
+                      <AvatarImage 
+                        className="object-cover"
+                        src={evaluation.evaluator_profile_image || "/avatar.png"}  
+                      />
+                    </Avatar>
+                    <span className="text-sm text-gray-500">{evaluation.evaluator_forename} {evaluation.evaluator_surname}, {formatDate(evaluation.comment_date)}</span>
+                  </div>
                 </div>
-                <div className="flex items-center gap-2">
-                  <Avatar className="border">
-                    <AvatarImage 
-                      className="object-cover"
-                      src={evaluation.evaluator_profile_image || "/avatar.png"}  
-                    />
-                  </Avatar>
-                  <span className="text-sm text-gray-500">{evaluation.evaluator_forename} {evaluation.evaluator_surname}, {formatDate(evaluation.comment_date)}</span>
-                </div>
-              </div>
+               </> 
+              ))}
             </div>
-          )) : (
+          ) : (
             <div className="text-sm">
               No commnets!
             </div>
